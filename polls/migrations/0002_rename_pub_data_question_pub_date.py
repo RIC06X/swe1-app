8300a30864a39bd139da2bfc545d9eb63ab47066
Yanjie Xu <yx2304@nyu.edu>
2022-03-07 21:08:09 -0500

    reformatted code and updated .travis.yml

--- polls/migrations/0002_rename_pub_data_question_pub_date.py
+++ polls/migrations/0002_rename_pub_data_question_pub_date.py
@@ -6,13 +6,13 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('polls', '0001_initial'),
+        ("polls", "0001_initial"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='question',
-            old_name='pub_data',
-            new_name='pub_date',
+            model_name="question",
+            old_name="pub_data",
+            new_name="pub_date",
         ),
     ]
--- polls/urls.py
+++ polls/urls.py
@@ -2,11 +2,11 @@ import imp
 from django.urls import path
 from . import views
 
-app_name = 'polls'
+app_name = "polls"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path("", views.index, name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
